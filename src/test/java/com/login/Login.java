0535b8cb8825f59d3d15ed798c88bb0f4a418de9
dorababuch1997 <dorababu5424@gmail.com>
2023-03-31 12:11:52 +0530

    orgin

--- src/test/java/com/login/Login.java
+++ src/test/java/com/login/Login.java
@@ -10,11 +10,6 @@ public class Login {
 		System.out.println("login merge");
 	}
 
-	public static void main(String[] args) {
-		
-		System.out.println("login ");
-		System.out.println("login Functionality");
-		System.out.println("login merge");
-	}
+	
 
 }
